9b908046f84bf17addd4dd4d3ca485abb6254097
jinxinking007 <32088824+jinxinkingG@users.noreply.github.com>
2024-03-09 10:59:08 +0800

    change the rootmarker

--- init.lua
+++ init.lua
@@ -39,9 +39,8 @@ return {
 		dependencies = { "williamboman/mason-lspconfig.nvim" },
 		opts = function(_, opts)
 			-- use this function notation to build some variables
-			-- local root_markers = { ".git", "mvnw", "gradlew", "build.gradle" }
-			-- local root_dir = require("jdtls.setup").find_root(root_markers)
-			local root_dir = vim.fn.getcwd()
+			local root_markers = { ".git", "mvnw", "gradlew", "build.gradle" }
+			local root_dir = require("jdtls.setup").find_root(root_markers)
 			-- calculate workspace dir
 			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 			local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
